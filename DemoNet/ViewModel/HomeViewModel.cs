312e5bee6538306490a0e60ea87d6ed9fc490792
Tú Võ Xuân <19522461@ms.uit.edu.vn>
2022-02-21 11:00:51 +0700

    Tu-buoc-4

---
+++ DemoNet/ViewModel/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoNet.ViewModel
+{
+    class HomeViewModel:BaseViewModel
+    {
+        public HomeViewModel()
+        {
+
+        }
+    }
+}
--- DemoNet/ViewModel/LoginViewModel.cs
+++ DemoNet/ViewModel/LoginViewModel.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using DemoNet.View;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DemoNet.ViewModel
 {
     class LoginViewModel : BaseViewModel
     {
+        public ICommand LoginCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
 
+        private string _Username = "";
+        public string Username { get => _Username; set { _Username = value; OnPropertyChanged(); } }
+        private string _Password = "";
+        public string Password { get => _Password; set { _Password = value; OnPropertyChanged(); } }
+
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand<LoginWindow>((p) => { return true; }, (p) => { Login(p); });
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { Password = p.Password; });
+        }
+
+        public void Login(LoginWindow loginWindow)
+        {
+            //MessageBox.Show(Username + "   " + Password);
+            if (DemoNet.Database.account.FindAccountByUsernameAndPassword(Username,Password))
+            {
+                HomeWindow homeWindow = new HomeWindow();
+                MessageBox.Show("Đăng nhập thành công!");
+                loginWindow.Hide();
+                homeWindow.ShowDialog();
+                loginWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không hợp lệ!");
+            }
+        }
     }
 }
